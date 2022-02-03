476555c831badf5ec747ed2641c8ef1874912503
rebe7716 <rebe7716@stud.kea.dk>
2022-02-03 13:41:57 +0100

    hej

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#000000",
+      secondary: "#010BEB",
+    },
+
+    fontFamily: {},
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
